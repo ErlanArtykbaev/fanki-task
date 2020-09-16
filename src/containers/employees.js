e135df8aed53f20378cd8e4b58683a4f534a26b8
erlan artykbaev <erik.2405@mail.ru>
2020-09-17 00:22:17 +0600

    fake data setup

--- src/containers/employees.js
+++ src/containers/employees.js
@@ -3,30 +3,37 @@ import lion from '../assets/img/lion.svg'
 export default [
   {
     img: lion,
-    name: 'Name Surname',
-    position: 'Employee-position',
-    date: 'dd month yyyy - dd month yyyy',
-    state: 'ill'
+    name: 'Никифорова Валентина',
+    position: 'Менеджер по продажам',
+    date: '10 августа 2020 - 21 августа 2020',
+    state: 'vacation'
   },
   {
     img: lion,
-    name: 'Name Surname',
-    position: 'Employee-position',
-    date: 'dd month yyyy - dd month yyyy',
-    state: 'ill'
+    name: 'Федотов Алексей',
+    position: 'Менеджер по продажам',
+    date: '20 августа 2020 - 25 августа 2020',
+    state: 'vacation'
   },
   {
     img: lion,
-    name: 'Name Surname',
-    position: 'Employee-position',
-    date: 'dd month yyyy - dd month yyyy',
-    state: 'ill'
+    name: 'Гончаров Леонид',
+    position: 'Менеджер по продажам',
+    date: '20 августа 2020 - 25 августа 2020',
+    state: 'vacation'
   },
   {
     img: lion,
-    name: 'Name Surname',
-    position: 'Employee-position',
-    date: 'dd month yyyy - dd month yyyy',
+    name: 'Яковлев Николай',
+    position: 'Менеджер по продажам',
+    date: '20 августа, 10:00 - 12:00',
+    state: 'dayOff'
+  },
+  {
+    img: lion,
+    name: 'Терентьев Валерий',
+    position: 'Менеджер по продажам',
+    date: '15 августа 2020 - 1 сентября 2020',
     state: 'dayOff'
   },
   {
@@ -38,30 +45,23 @@ export default [
   },
   {
     img: lion,
-    name: 'Name Surname',
-    position: 'Employee-position',
-    date: 'dd month yyyy - dd month yyyy',
-    state: 'dayOff'
+    name: 'Эрлан Артыкбаев',
+    position: 'Senior developer',
+    date: '20 июля 2020 - 30 июля 2020',
+    state: 'ill'
+  },
+  {
+    img: lion,
+    name: 'Лазарев Буткин',
+    position: 'junior developer',
+    date: '12 сентября 2021 - 22 сентября 2021',
+    state: 'ill'
   },
   {
     img: lion,
     name: 'Name Surname',
     position: 'Employee-position',
     date: 'dd month yyyy - dd month yyyy',
-    state: 'vacation'
-  },
-  {
-    img: lion,
-    name: 'Name Surname',
-    position: 'Employee-position',
-    date: 'dd month yyyy - dd month yyyy',
-    state: 'vacation'
-  },
-  {
-    img: lion,
-    name: 'Name Surname',
-    position: 'Employee-position',
-    date: 'dd month yyyy - dd month yyyy',
-    state: 'vacation'
+    state: 'ill'
   }
 ]
