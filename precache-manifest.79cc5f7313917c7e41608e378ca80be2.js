38853eae42622afbfdd3c8d5a4e6695e8929b57c
erlan artykbaev <erik.2405@mail.ru>
2020-12-11 10:47:52 +0600

    Updates

---
+++ precache-manifest.79cc5f7313917c7e41608e378ca80be2.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4b2461f7464a618db25c2e67f9d75dd",
+    "url": "/fanki-task/index.html"
+  },
+  {
+    "revision": "5aae05efd8cf8369dd6b",
+    "url": "/fanki-task/static/css/main.715b0bbc.chunk.css"
+  },
+  {
+    "revision": "3c2197b1ab77921ab098",
+    "url": "/fanki-task/static/js/2.013fae19.chunk.js"
+  },
+  {
+    "revision": "0dc713a05373e5c9a88cb999f31ae6e4",
+    "url": "/fanki-task/static/js/2.013fae19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5aae05efd8cf8369dd6b",
+    "url": "/fanki-task/static/js/main.321cda37.chunk.js"
+  },
+  {
+    "revision": "c26e5cbf1141efe351c2",
+    "url": "/fanki-task/static/js/runtime-main.a12f0e06.js"
+  },
+  {
+    "revision": "288297ba47a6127f3e305a89b77e1a0c",
+    "url": "/fanki-task/static/media/Smile.288297ba.svg"
+  },
+  {
+    "revision": "e63f9d1f209634fb40785e6068329487",
+    "url": "/fanki-task/static/media/avatar.e63f9d1f.svg"
+  },
+  {
+    "revision": "e961644286f64f08d9359cc709433ddf",
+    "url": "/fanki-task/static/media/book-open.e9616442.svg"
+  },
+  {
+    "revision": "6461167e3ec9172b567d3ca07f70221c",
+    "url": "/fanki-task/static/media/book.6461167e.svg"
+  },
+  {
+    "revision": "417fe29bc5a5641c89ee87707ebe69d5",
+    "url": "/fanki-task/static/media/calendar.417fe29b.svg"
+  },
+  {
+    "revision": "522dd4dd755c71a97b22809aa24d8f0e",
+    "url": "/fanki-task/static/media/checkmark-circle-2.522dd4dd.svg"
+  },
+  {
+    "revision": "67c4eb0f32dbff35b3c7d36330f7849d",
+    "url": "/fanki-task/static/media/chevron-left.67c4eb0f.svg"
+  },
+  {
+    "revision": "e1deec2c1750a152eb2617a62dda1411",
+    "url": "/fanki-task/static/media/chevron-right.e1deec2c.svg"
+  },
+  {
+    "revision": "bf7423fed0e6f143a0aba2e2ddacb47d",
+    "url": "/fanki-task/static/media/copy.bf7423fe.svg"
+  },
+  {
+    "revision": "374d8c9ab2996c49a9a4bb82f24e9095",
+    "url": "/fanki-task/static/media/fanki-active.374d8c9a.svg"
+  },
+  {
+    "revision": "79ce52bd7512ce414959c78dec88c540",
+    "url": "/fanki-task/static/media/flash.79ce52bd.svg"
+  },
+  {
+    "revision": "2645854690ef21563ab1930e32ff2ec0",
+    "url": "/fanki-task/static/media/image-8.26458546.svg"
+  },
+  {
+    "revision": "2531c1f5e55a509d0ebed354664b48bd",
+    "url": "/fanki-task/static/media/layers.2531c1f5.svg"
+  },
+  {
+    "revision": "f556e7daa7a5e544420cdc7d2e8b7c80",
+    "url": "/fanki-task/static/media/lion.f556e7da.svg"
+  },
+  {
+    "revision": "7e6348de6d888f6ae9bc610c72d6cd02",
+    "url": "/fanki-task/static/media/message-circle.7e6348de.svg"
+  },
+  {
+    "revision": "54f17a87b0180c5497abf1e563303596",
+    "url": "/fanki-task/static/media/news-img.54f17a87.png"
+  },
+  {
+    "revision": "a00dad45f3764ea909b6ef8b80bcbc16",
+    "url": "/fanki-task/static/media/people.a00dad45.svg"
+  },
+  {
+    "revision": "68d9e8dffd01c8deecb12fcf878f2a09",
+    "url": "/fanki-task/static/media/question-mark-circle.68d9e8df.svg"
+  },
+  {
+    "revision": "674ee71d47dd79caee65c4041a42898c",
+    "url": "/fanki-task/static/media/settings.674ee71d.svg"
+  },
+  {
+    "revision": "a8d4dfdf54d6514c8dad6b6b383ca644",
+    "url": "/fanki-task/static/media/trending-up.a8d4dfdf.svg"
+  }
+]);
